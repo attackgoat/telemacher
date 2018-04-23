502856cc9ae39b1f99b03708cb496b8b0c999d32
john wells <john@verylargebits.com>
2018-04-22 20:27:37 -0700

    Add basic api

--- src/chat.rs
+++ src/chat.rs
@@ -1,18 +1,50 @@
-enum Request {
-    Join(Join),
-    Message(Message),
-}
-
-struct Join {
+pub struct Join {
     name: String,
     user_id: u64,
 }
 
-struct Message {
+impl Join {
+    pub fn new<N: Into<String>>(user_id: u64, name: N) -> Self {
+        Self {
+            name: name.into(),
+            user_id: user_id,
+        }
+    }
+}
+
+pub struct Message {
     text: String,
     user_id: u64,
 }
 
-struct Response {
-    messages: Vec<String>,
+impl Message {
+    pub fn new<N: Into<String>>(user_id: u64, text: N) -> Self {
+        Self {
+            text: text.into(),
+            user_id: user_id,
+        }
+    }
+}
+
+pub enum Request {
+    Join(Join),
+    Message(Message),
+}
+
+pub struct Server {
+
+}
+
+impl Server {
+    pub fn respond(request: &Request) -> String {
+
+    }
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        Self {
+
+        }
+    }
 }
